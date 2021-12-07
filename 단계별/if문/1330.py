37b887c2b6a0b84a3b27d49c3002ea7322abac81
leechi2 <chang2595@naver.com>
2021-12-07 17:22:01 +0900

    단계별 - if문

---
+++ 단계별/if문/1330.py
@@ -0,0 +1,8 @@
+a,b = map(int,input().split())
+if a>=b:
+    if a>b:
+        print(">")
+    else:
+        print("==")
+else:
+    print("<")
---
+++ 단계별/if문/2884.py
@@ -0,0 +1,8 @@
+h,m = map(int, input().split())
+if m >= 45:
+    print("%d %d" % (h, m-45))
+else:
+    if h==0:
+        print(23 , m+15)
+    else:
+        print("%d %d" % (h-1, m + 15))
